e921d7f85d46630745062edb64ea39054798bce0
Colton Asnes <coltonasnes@gmail.com>
2025-07-27 17:43:56 -0500

    create /public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         <div className="nav-container">
           <Link to="/" className="nav-logo logo-container" aria-label="ChronoBid Home">
             <div className="logo-content">
-              <img src="/Chrono Bid Logo.png" alt="ChronoBid Logo" className="logo-image" />
+              <img src="/public/Chrono Bid Logo.png" alt="ChronoBid Logo" className="logo-image" />
               <span className="logo-text">ChronoBid</span>
             </div>
           </Link>
